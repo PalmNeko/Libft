3d6f7c2c7c315e0a9c282af31fd9ff738f3b04dc
tookuyam <tookuyam@student.42tokyo.jp>
2024-03-09 21:12:24 +0900

    add: ft_lstadd_front

---
+++ tests/src/ft_lstadd_front_bonus_test.c
@@ -0,0 +1,21 @@
+
+#include "tyctest.h"
+#include "libft.h"
+#include <limits.h>
+#include <errno.h>
+
+TEST(ft_lstadd_front_bonus, basic_usage) {
+	t_list	*root;
+
+	root = (t_list[]){{.content=(int []){1}, .next = NULL}};
+	ft_lstadd_front(&root, (t_list[]){{.content=(int []){2}, .next = NULL}});
+	EXPECT_EQ(2, *(int *)root->content);
+	ft_lstadd_front(&root, (t_list[]){{.content=(int []){3}, .next = NULL}});
+	EXPECT_EQ(3, *(int *)root->content);
+	EXPECT_EQ(2, *(int *)root->next->content);
+	EXPECT_EQ(1, *(int *)root->next->next->content);
+	root = NULL;
+	ft_lstadd_front(&root, (t_list[]){{.content=(int []){1}, .next = NULL}});
+	EXPECT_EQ(1, *(int *)root->content);
+}
+
