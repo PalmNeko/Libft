eae7ccb2ae753cda82ad5350794f823332f40ecc
tookuyam <tookuyam@student.42tokyo.jp>
2024-03-11 01:33:35 +0900

    add: ft_lstiter_bonus_test

---
+++ tests/src/ft_lstiter_bonus_test.c
@@ -0,0 +1,36 @@
+
+#include "tyctest.h"
+#include "libft.h"
+#include <limits.h>
+#include <errno.h>
+
+static int	g_value = 0;
+static void	count_up(void *content)
+{
+	if (content == NULL)
+		g_value = 0;
+	else
+	{
+		ft_putnbr_fd(*(int *)content, 1);
+		g_value += *(int *)content;
+	}
+	return ;
+}
+
+TEST(ft_lstiter, basic_usage) {
+	t_list	*root;
+
+	root = NULL;
+	ft_lstiter(root, count_up);
+	EXPECT_EQ(0, g_value);
+	root = (t_list []) {{
+		.content = (int []){1},
+		.next = (t_list	[]) {{
+			.content = (int []){2},
+			.next = NULL,
+		}}
+	}};
+	ft_lstiter(root, count_up);
+	EXPECT_EQ(3, g_value);
+}
+
