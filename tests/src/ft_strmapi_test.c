4106a62c3d955cea70e72461beb79a8b7ceec376
tookuyam <tookuyam@student.42.tokyo.jp>
2024-03-11 17:20:21 +0900

    add: ft_strmapi test

---
+++ tests/src/ft_strmapi_test.c
@@ -0,0 +1,30 @@
+
+#include "tyctest.h"
+#include "libft.h"
+#include <limits.h>
+#include <errno.h>
+
+static int	g_cnt;
+static unsigned int	g_indexes[6];
+
+static char	_toupper_iteri(unsigned int index, char target)
+{
+	if (g_cnt >= 6)
+		return ('\0');
+	g_indexes[g_cnt] = index;
+	g_cnt++;
+	return (ft_toupper(target));
+}
+
+TEST(ft_strmapi, basic_usage) {
+	char	*result;
+
+	result = ft_strmapi("abcde", _toupper_iteri);
+	EXPECT_STREQ("ABCDE", result);
+	EXPECT_EQ((unsigned int)0, g_indexes[0]);
+	EXPECT_EQ((unsigned int)1, g_indexes[1]);
+	EXPECT_EQ((unsigned int)2, g_indexes[2]);
+	EXPECT_EQ((unsigned int)3, g_indexes[3]);
+	EXPECT_EQ((unsigned int)4, g_indexes[4]);
+	EXPECT_EQ(5, g_cnt);
+}
