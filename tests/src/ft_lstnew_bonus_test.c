51a776d61f00ef1ecaa929af07ae5e33fc245a2a
tookuyam <tookuyam@student.42.tokyo.jp>
2024-03-11 13:39:32 +0900

    add: ft_lstnew test

---
+++ tests/src/ft_lstnew_bonus_test.c
@@ -0,0 +1,13 @@
+
+#include "tyctest.h"
+#include "libft.h"
+#include <limits.h>
+#include <errno.h>
+
+TEST(ft_lstnew_bonus, basic_usage) {
+	t_list	*root;
+
+	root = ft_lstnew((int []){1});
+	ASSERT_NE(NULL, root);
+	EXPECT_EQ(1, *(int *)root->content);
+}
