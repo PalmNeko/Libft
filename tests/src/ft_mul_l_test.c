0564b4e580256b6c3d6b55bfa680afebdb064266
tookuyam <tookuyam@student.42.tokyo.jp>
2024-03-11 15:47:08 +0900

    add: ft_mul_l test

---
+++ tests/src/ft_mul_l_test.c
@@ -0,0 +1,23 @@
+
+#include "tyctest.h"
+#include "libft.h"
+#include <limits.h>
+#include <errno.h>
+
+TEST(ft_mul_l, basic_usage) {
+	EXPECT_EQ(0, ft_mul_l(0, 0));
+	EXPECT_EQ(0, ft_mul_l(30000000, 0));
+	EXPECT_EQ(256, ft_mul_l(128, 2));
+	EXPECT_EQ((LONG_MAX - 1), ft_mul_l((LONG_MAX - 1) / 2, 2));
+	EXPECT_EQ(LONG_MIN, ft_mul_l(LONG_MIN / 2, 2));
+}
+
+TEST(ft_mul_l, error_check) {
+	errno = 0;
+	EXPECT_EQ(LONG_MAX, ft_mul_l((LONG_MAX / 2) + 1, 2));
+	EXPECT_EQ(ERANGE, errno);
+	errno = 0;
+	EXPECT_EQ(LONG_MIN, ft_mul_l((LONG_MIN / 2) - 1, 2));
+	EXPECT_EQ(ERANGE, errno);
+	errno = 0;
+}
