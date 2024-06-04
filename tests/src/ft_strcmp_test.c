24cbfdbf82078a73032b56317c5f1f3a5666502f
tookuyam <tookuyam@student.42.tokyo.jp>
2024-06-04 13:50:27 +0900

    add: ft_strcmp tests

---
+++ tests/src/ft_strcmp_test.c
@@ -0,0 +1,19 @@
+
+#include "tyctest.h"
+#include "libft.h"
+#include <limits.h>
+#include <errno.h>
+
+TEST(ft_strcmp, basic_usage) {
+	char	src[] = "abcdefgh";
+
+	EXPECT_EQ(0, ft_strcmp("", ""));
+	EXPECT_EQ(0, ft_strcmp("a", "a"));
+	EXPECT_EQ(0, ft_strcmp("abc", "abc"));
+	EXPECT_NE(0, ft_strcmp(" abc", "abc"));
+	EXPECT_NE(0, ft_strcmp("abc ", "abc"));
+	EXPECT_NE(0, ft_strcmp("abc", "abc "));
+	EXPECT_NE(0, ft_strcmp("abc", " abc"));
+	EXPECT_NE(0, ft_strcmp("abc", ""));
+	EXPECT_NE(0, ft_strcmp("", " abc"));
+}
