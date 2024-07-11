2e43f99a71f086e55725b9f4acc1ff735bd9a499
tookuyam <tookuyam@42.student.fr>
2024-07-11 16:53:01 +0900

    fix: for not found

--- submit/ft_getenv.c
+++ submit/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 18:57:23 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/11 15:25:01 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/11 16:52:21 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_getenv(const char *name)
 	while (environ[index] != NULL)
 	{
 		find_iter = ft_strchr(environ[index], '=');
-		if (find_iter == NULL && ft_strncmp(
+		if (find_iter != NULL && ft_strncmp(
 				name, environ[index], find_iter - environ[index]) == 0)
 			break ;
 		index++;
