71257fb25fde2769d086d91632085e4429ec398d
PalmNeko <tookuyam@student.42tokyo.fr>
2024-07-16 21:02:05 +0900

    add: for circular_list

--- tests/src/ft_lstiter_bonus_test.c
+++ tests/src/ft_lstiter_bonus_test.c
@@ -20,6 +20,7 @@ TEST(ft_lstiter, basic_usage) {
 	t_list	*root;
 
 	root = NULL;
+	count_up(NULL);
 	ft_lstiter(root, count_up);
 	EXPECT_EQ(0, g_value);
 	root = (t_list []) {{
@@ -33,3 +34,22 @@ TEST(ft_lstiter, basic_usage) {
 	EXPECT_EQ(3, g_value);
 }
 
+TEST(ft_lstiter, circular_list) {
+	t_list	*root;
+
+	root = NULL;
+	count_up(NULL);
+	ft_lstiter(root, count_up);
+	EXPECT_EQ(0, g_value);
+	root = (t_list []) {{
+		.content = (int []){1},
+		.next = NULL,
+	}};
+	ft_lstadd_front(&root, (t_list []) {{
+		.content = (int []){2},
+		.next = NULL,
+		.prev = NULL,
+	}});
+	ft_lstiter(root, count_up);
+	EXPECT_EQ(3, g_value);
+}
