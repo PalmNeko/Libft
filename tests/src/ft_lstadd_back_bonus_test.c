bd31b1e4694d350aa98d3a7db0487e0ff9fc1949
tookuyam <tookuyam@42.student.fr>
2024-07-17 16:19:15 +0900

    add: for two-way list

--- tests/src/ft_lstadd_back_bonus_test.c
+++ tests/src/ft_lstadd_back_bonus_test.c
@@ -17,3 +17,32 @@ TEST(ft_lstadd_back_bonus, basic_usage) {
 	ft_lstadd_back(&root, (t_list[]){{.content=(int []){1}, .next = NULL}});
 	EXPECT_EQ(1, *(int *)root->content);
 }
+
+TEST(ft_lstadd_back_bonus, two_way_list) {
+	t_list	*root;
+
+	root = ft_lstnew((int []){1});
+	ft_lstadd_back(&root, ft_lstnew((int []){2}));
+	EXPECT_EQ(2, *(int *)root->next->content);
+	EXPECT_EQ(1, *(int *)root->next->prev->content);
+	ft_lstadd_back(&root, ft_lstnew((int []){3}));
+	EXPECT_EQ(3, *(int *)root->next->next->content);
+	EXPECT_EQ(1, *(int *)root->next->next->prev->prev->content);
+	EXPECT_EQ(NULL, root->next->next->next);
+	ft_lstclear(&root, (void (*)(void *))ft_voidnop);
+	root = NULL;
+	ft_lstadd_back(&root, ft_lstnew((int []){1}));
+	EXPECT_EQ(1, *(int *)root->content);
+	EXPECT_EQ(NULL, root->next);
+	EXPECT_EQ(NULL, root->prev);
+	ft_lstadd_back(&root, ft_lstnew((int []){2}));
+	root = root->next;
+	ft_lstadd_back(&root, ft_lstnew((int []){3}));
+	EXPECT_EQ(2, *(int *)root->content);
+	EXPECT_EQ(3, *(int *)root->next->content);
+	EXPECT_EQ(NULL, root->next->next);
+	EXPECT_EQ(1, *(int *)root->prev->content);
+	EXPECT_EQ(NULL, root->prev->prev);
+	root = root->prev;
+	ft_lstclear(&root, (void (*)(void *))ft_voidnop);
+}
