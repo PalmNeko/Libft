ee23cabf89c4daefba772ab41ff9113e9fa702c8
tookuyam <tookuyam@42.student.fr>
2024-07-17 16:29:43 +0900

    add: ft_lstdelone_bonus test for two_way_list

--- tests/src/ft_lstdelone_bonus_test.c
+++ tests/src/ft_lstdelone_bonus_test.c
@@ -17,6 +17,7 @@ TEST(ft_lstdelone_bonus, basic_usage) {
 	t_list	*tmp;
 	int		values[3] = {1, 2, 3};
 
+	g_del_cnt = 0;
 	root = NULL;
 	ft_lstadd_front(&root, ft_lstnew(&values[0]));
 	ft_lstadd_front(&root, ft_lstnew(&values[1]));
@@ -32,6 +33,34 @@ TEST(ft_lstdelone_bonus, basic_usage) {
 	ft_lstdelone(tmp, _delone_);
 	EXPECT_EQ(3, g_del_cnt);
 	EXPECT_EQ(NULL, root);
+	EXPECT_EQ(4, values[0]);
+	EXPECT_EQ(4, values[1]);
+	EXPECT_EQ(4, values[2]);
+}
+
+TEST(ft_lstdelone_bonus, two_way_list) {
+	t_list	*root;
+	t_list	*tmp;
+	int		values[3] = {1, 2, 3};
+
+	g_del_cnt = 0;
+	root = NULL;
+	ft_lstadd_front(&root, ft_lstnew(&values[0]));
+	ft_lstadd_front(&root, ft_lstnew(&values[1]));
+	ft_lstadd_front(&root, ft_lstnew(&values[2]));
+	tmp = root->next;
+	ft_lstdelone(tmp, _delone_);
+	EXPECT_EQ(1, *(int *)root->next->content);
+	EXPECT_EQ(3, *(int *)root->next->prev->content);
+	tmp = root;
+	root = root->next;
+	ft_lstdelone(tmp, _delone_);
+	EXPECT_EQ(1, *(int *)root->content);
+	EXPECT_EQ(NULL, root->prev);
+	EXPECT_EQ(NULL, root->next);
+	tmp = root;
+	ft_lstdelone(tmp, _delone_);
+	EXPECT_EQ(3, g_del_cnt);
 	EXPECT_EQ(4, values[0]);
 	EXPECT_EQ(4, values[1]);
 	EXPECT_EQ(4, values[2]);
