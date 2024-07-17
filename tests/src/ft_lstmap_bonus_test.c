9258ecc1a3fec3e9dec5541e7832586da0db45d3
tookuyam <tookuyam@42.student.fr>
2024-07-17 16:49:20 +0900

    add: for two_way list

--- tests/src/ft_lstmap_bonus_test.c
+++ tests/src/ft_lstmap_bonus_test.c
@@ -8,13 +8,10 @@ static int	g_called_cnt;
 static void	*_squre(void *value)
 {
 	static int	values[3];
-	static int	cnt;
 
-	values[cnt] = (*(int *)value) * (*(int *)value);
-	cnt++;
+	values[g_called_cnt] = (*(int *)value) * (*(int *)value);
 	g_called_cnt++;
-
-	return (&values[cnt - 1]);
+	return (&values[g_called_cnt - 1]);
 }
 
 
@@ -40,4 +37,24 @@ TEST(ft_lstmap_bonus, basic_usage)
 	EXPECT_EQ(1, *(int *)squared_root->content);
 	EXPECT_EQ(9, *(int *)squared_root->next->content);
 	ft_lstclear(&squared_root, _del_func);
+}
+
+TEST(ft_lstmap_bonus, two_way_list)
+{
+	t_list	*root;
+	t_list	*squared_root;
+
+	root = NULL;
+	ft_lstadd_back(&root, ft_lstnew((int []){1}));
+	ft_lstadd_back(&root, ft_lstnew((int []){3}));
+	g_called_cnt = 0;
+	squared_root = ft_lstmap(root, _squre, _del_func);
+	EXPECT_EQ(2, g_called_cnt);
+	EXPECT_EQ(1, *(int *)squared_root->content);
+	EXPECT_EQ(1, *(int *)squared_root->next->prev->content);
+	EXPECT_EQ(9, *(int *)squared_root->next->content);
+	EXPECT_EQ(NULL, squared_root->next->next);
+	EXPECT_EQ(NULL, squared_root->prev);
+	ft_lstclear(&squared_root, _del_func);
+	ft_lstclear(&root, _del_func);
 }
