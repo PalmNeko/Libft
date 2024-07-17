fc09b7baf5129f52425ef4e7c446ca9516003992
tookuyam <tookuyam@42.student.fr>
2024-07-17 16:33:20 +0900

    add: test for two_way_list

--- tests/src/ft_lstnew_bonus_test.c
+++ tests/src/ft_lstnew_bonus_test.c
@@ -12,3 +12,14 @@ TEST(ft_lstnew_bonus, basic_usage) {
 	EXPECT_EQ(1, *(int *)root->content);
 	ft_lstdelone(root, (void(*)(void *))ft_voidnop);
 }
+
+TEST(ft_lstnew_bonus, two_way_list) {
+	t_list	*root;
+
+	root = ft_lstnew((int []){1});
+	ASSERT_NE(NULL, root);
+	EXPECT_EQ(1, *(int *)root->content);
+	EXPECT_EQ(NULL, root->next);
+	EXPECT_EQ(NULL, root->prev);
+	ft_lstdelone(root, (void(*)(void *))ft_voidnop);
+}
