1d5ff14fcd4071ecdc731c5fade5287ff3b5c0d0
tookuyam <tookuyam@42.student.fr>
2024-07-17 16:04:52 +0900

    add: test for two-way list

--- tests/src/ft_array_to_lst_test.c
+++ tests/src/ft_array_to_lst_test.c
@@ -20,3 +20,18 @@ TEST(ft_array_to_lst, basic_usage) {
 	ft_lstclear(&lst, (void (*)())ft_voidnop);
 }
 
+TEST(ft_array_to_lst, two_way) {
+	t_list	*lst;
+
+	lst = ft_array_to_lst((void *[]){
+		(int []){1},
+		(int []){2},
+		(int []){3},
+	}, 3);
+	ASSERT_NE(NULL, lst);
+	EXPECT_EQ(ft_lstsize(lst), 3);
+	EXPECT_EQ(*(int *)lst->content, 1);
+	EXPECT_EQ(*(int *)lst->next->prev->content, 1);
+	EXPECT_EQ(*(int *)lst->next->next->prev->prev->content, 1);
+	ft_lstclear(&lst, (void (*)())ft_voidnop);
+}
