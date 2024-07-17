386c651a3aa244be5993e8d35a01b3000a709909
tookuyam <tookuyam@42.student.fr>
2024-07-17 16:07:06 +0900

    add: for two-way list

--- tests/src/ft_lst_to_array_test.c
+++ tests/src/ft_lst_to_array_test.c
@@ -27,3 +27,25 @@ TEST(ft_lst_to_array, basic_usage)
 	free(values);
 	ft_lstclear(&lst, (void(*)(void *))ft_voidnop);
 }
+
+TEST(ft_lst_to_array, two_way_list)
+{
+	t_list	*lst;
+	int		**values;
+	size_t	size;
+
+	lst = ft_array_to_lst((void *[]){
+		(int []){1},
+		(int []){2},
+		(int []){3},
+	}, 3);
+	ASSERT_NE(lst, NULL);
+	values = (int **)ft_lst_to_array(lst, &size);
+	ASSERT_NE(values, NULL);
+	EXPECT_TRUE(size == 3);
+	EXPECT_EQ(*values[0], 1);
+	EXPECT_EQ(*values[1], 2);
+	EXPECT_EQ(*values[2], 3);
+	free(values);
+	ft_lstclear(&lst, (void(*)(void *))ft_voidnop);
+}
