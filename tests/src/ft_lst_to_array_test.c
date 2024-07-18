72be5172606808479240b09c6bfb591ea8a2f871
tookuyam <tookuyam@42.student.fr>
2024-07-18 15:20:40 +0900

    add: test for circular list

--- tests/src/ft_lst_to_array_test.c
+++ tests/src/ft_lst_to_array_test.c
@@ -49,3 +49,22 @@ TEST(ft_lst_to_array, two_way_list)
 	free(values);
 	ft_lstclear(&lst, (void(*)(void *))ft_voidnop);
 }
+
+TEST(ft_lst_to_array, circular_list)
+{
+	t_list	*lst;
+	int		**values;
+	size_t	size;
+
+	lst = ft_lstnewcircular((int []){1});
+	ft_lstadd_back(&lst, ft_lstnew((int []){2}));
+	ft_lstadd_back(&lst, ft_lstnew((int []){3}));
+	values = (int **)ft_lst_to_array(lst, &size);
+	ASSERT_NE(values, NULL);
+	EXPECT_TRUE(size == 3);
+	EXPECT_EQ(*values[0], 1);
+	EXPECT_EQ(*values[1], 2);
+	EXPECT_EQ(*values[2], 3);
+	free(values);
+	ft_lstclear(&lst, (void(*)(void *))ft_voidnop);
+}
