6223e5690c450fad8a9b121c1b86ad6b8bdd4454
tookuyam <tookuyam@42.student.fr>
2024-07-18 15:35:10 +0900

    add: test for circular list

--- tests/src/ft_lstmap_bonus_test.c
+++ tests/src/ft_lstmap_bonus_test.c
@@ -58,3 +58,23 @@ TEST(ft_lstmap_bonus, two_way_list)
 	ft_lstclear(&squared_root, _del_func);
 	ft_lstclear(&root, _del_func);
 }
+
+TEST(ft_lstmap_bonus, circular_list)
+{
+	t_list	*root;
+	t_list	*squared_root;
+
+	root = NULL;
+	ft_lstadd_back(&root, ft_lstnewcircular((int []){1}));
+	ft_lstadd_back(&root, ft_lstnew((int []){3}));
+	g_called_cnt = 0;
+	squared_root = ft_lstmap(root, _squre, _del_func);
+	EXPECT_EQ(2, g_called_cnt);
+	EXPECT_EQ(1, *(int *)squared_root->content);
+	EXPECT_EQ(1, *(int *)squared_root->next->prev->content);
+	EXPECT_EQ(9, *(int *)squared_root->next->content);
+	EXPECT_EQ(NULL, squared_root->next->next);
+	EXPECT_EQ(NULL, squared_root->prev);
+	ft_lstclear(&squared_root, _del_func);
+	ft_lstclear(&root, _del_func);
+}
