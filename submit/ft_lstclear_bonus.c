d64ef6838385eb9a65c28bb7e6df85da81a990d0
tookuyam <tookuyam@42.student.fr>
2024-07-18 14:28:54 +0900

    fix: for circular list

--- submit/ft_lstclear_bonus.c
+++ submit/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 19:25:51 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/18 12:44:01 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/18 14:26:17 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,29 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list	*del_lst;
+	t_list	*itr;
 	t_list	*free_lst;
+	t_list	*start;
 
 	if (lst == NULL)
 		return ;
-	if ((*lst)->prev != NULL)
-		(*lst)->prev->next = NULL;
-	del_lst = (*lst)->next;
-	while (del_lst != NULL)
+	itr = (*lst);
+	if (itr->prev != NULL)
 	{
-		free_lst = del_lst;
-		del_lst = del_lst->next;
+		itr = itr->prev;
+		itr->next->prev = NULL;
+	}
+	while (itr->prev != NULL)
+		itr = itr->prev;
+	start = itr;
+	itr = itr->next;
+	while (itr != NULL && itr != start)
+	{
+		free_lst = itr;
+		itr = itr->next;
 		ft_lstdelone(free_lst, del);
 	}
-	del_lst = (*lst);
-	while (del_lst != NULL)
-	{
-		free_lst = del_lst;
-		del_lst = del_lst->prev;
-		ft_lstdelone(free_lst, del);
-	}
+	ft_lstdelone(start, del);
 	*lst = NULL;
 	return ;
 }
