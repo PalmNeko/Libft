c57e76e62f5f564c8c5702335ed830518494fb5b
tookuyam <tookuyam@42.student.fr>
2024-07-18 12:44:42 +0900

    fix: for two_way list

--- submit/ft_lstclear_bonus.c
+++ submit/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 19:25:51 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/16 15:42:02 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:44:01 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,13 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		del_lst = del_lst->next;
 		ft_lstdelone(free_lst, del);
 	}
-	ft_lstdelone(*lst, del);
+	del_lst = (*lst);
+	while (del_lst != NULL)
+	{
+		free_lst = del_lst;
+		del_lst = del_lst->prev;
+		ft_lstdelone(free_lst, del);
+	}
 	*lst = NULL;
 	return ;
 }
