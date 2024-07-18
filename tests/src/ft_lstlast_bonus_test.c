147fc3f5e66e607a7ec3f286d8ff6ed61c0368e9
tookuyam <tookuyam@42.student.fr>
2024-07-18 12:49:08 +0900

    add: for two_way list

--- tests/src/ft_lstlast_bonus_test.c
+++ tests/src/ft_lstlast_bonus_test.c
@@ -19,3 +19,17 @@ TEST(ft_lstlast_bonus, basic_usage) {
 	EXPECT_EQ(NULL, ft_lstlast(NULL));
 }
 
+TEST(ft_lstlast_bonus, two_way_list) {
+	t_list	*root;
+
+	root = NULL;
+	ft_lstadd_back(&root, ft_lstnew((int []){1}));
+	ft_lstadd_back(&root, ft_lstnew((int []){2}));
+	ft_lstadd_back(&root, ft_lstnew((int []){3}));
+	EXPECT_EQ(3, *(int *)ft_lstlast(root)->content);
+	EXPECT_EQ(3, *(int *)ft_lstlast(root->next)->content);
+	EXPECT_EQ(3, *(int *)ft_lstlast(root->next->next)->content);
+	EXPECT_EQ(NULL, ft_lstlast(root)->next);
+	EXPECT_EQ(NULL, ft_lstlast(NULL));
+	ft_lstclear(&root, (void(*)())ft_voidnop);
+}
