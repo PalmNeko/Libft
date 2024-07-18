4378b9479af9b3666cd3fb40a7d7e2a2e8541cc1
tookuyam <tookuyam@42.student.fr>
2024-07-18 16:04:50 +0900

    fix: test for circular list

--- tests/src/ft_lstadd_front_bonus_test.c
+++ tests/src/ft_lstadd_front_bonus_test.c
@@ -39,23 +39,23 @@ TEST(ft_lstadd_front_bonus, two_way_list) {
 
 TEST(ft_lstadd_front_bonus, circular_list) {
 	t_list	*root;
-	t_list	*tmp;
 
-	root = (t_list[]){{.content=(int []){1}, .next = NULL, .prev = NULL}};
-	root->next = root;
-	root->prev = root;
-	ft_lstadd_front(&root, (t_list[]){{.content=(int []){2}, .next = NULL, .prev = NULL}});
+	root = ft_lstnewcircular((int []){1});
+	ft_lstadd_front(&root, ft_lstnew((int []){2}));
 	EXPECT_EQ(2, *(int *)root->content);
 	EXPECT_EQ(1, *(int *)root->next->content);
 	EXPECT_EQ(2, *(int *)root->next->next->content);
 	EXPECT_EQ(1, *(int *)root->prev->content);
 	EXPECT_EQ(2, *(int *)root->prev->prev->content);
 	EXPECT_EQ(root, root->prev->prev);
-	tmp = root;
 	root = root->next;
-	ft_lstadd_front(&root, (t_list[]){{.content=(int []){3}, .next = NULL, .prev = NULL}});
-	EXPECT_EQ(3, *(int *)tmp->prev->content);
-	EXPECT_EQ(1, *(int *)tmp->prev->prev->content);
-	EXPECT_EQ(2, *(int *)tmp->prev->prev->prev->content);
-	EXPECT_EQ(tmp, tmp->prev->prev->prev);
+	ft_lstadd_front(&root, ft_lstnew((int []){3}));
+	EXPECT_EQ(3, *(int *)root->content);
+	EXPECT_EQ(2, *(int *)root->prev->content);
+	EXPECT_EQ(1, *(int *)root->prev->prev->content);
+	EXPECT_EQ(3, *(int *)root->prev->prev->prev->content);
+	EXPECT_EQ(1, *(int *)root->next->content);
+	EXPECT_EQ(2, *(int *)root->next->next->content);
+	EXPECT_EQ(3, *(int *)root->next->next->next->content);
+	ft_lstclear(&root, (void (*)())ft_voidnop);
 }
